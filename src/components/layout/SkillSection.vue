cb3b1a74bbbcbfc1d64eacb6d125c05cf7e2d1dd
midoon <harunkoding@gmail.com>
2025-09-25 06:29:06 +0700

    skill car

--- src/components/layout/SkillSection.vue
+++ src/components/layout/SkillSection.vue
@@ -1,34 +1,113 @@
 <template>
-  <section id="skills" class="bg-accent-background"></section>
+  <section id="skills" class="bg-accent-background">
+    <div class="grid grid-cols-2 lg:max-w-[80vw] pb-20">
+      <!-- card skill -->
+      <div v-for="skill in skills" :key="skill.name" class="p-4">
+        <div
+          class="bg-primary-background rounded-lg p-6 flex flex-col items-center text-center h-full hover:scale-105 transition-transform duration-300"
+        >
+          <img :src="skill.icon" :alt="skill.name" class="w-16 h-16 mb-4" />
+          <h3 class="text-xl font-semibold mb-2 text-primary-text">
+            {{ skill.name }}
+          </h3>
+          <p class="text-secondary-text">{{ skill.description }}</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
-<script>
+<script setup>
 const skills = [
-  { name: "Javascript", description: "Programming Language", icon: "" },
-  { name: "Node.Js", description: "Javascript runtime", icon: "" },
-  { name: "Golang", description: "Programming language", icon: "" },
-  { name: "PHP", description: "Programming language", icon: "" },
-  { name: "Python", description: "Programming language", icon: "" },
-  { name: "Express.JS", description: "Javascript Framework", icon: "" },
-  { name: "Fiber", description: "Golang Framework", icon: "" },
-  { name: "Laravel", description: "PHP Framework", icon: "" },
-  { name: "Flask", description: "Python Framework", icon: "" },
-  { name: "Vue.JS", description: "Javascript Framework", icon: "" },
-  { name: "HTML", description: "Markup Framework", icon: "" },
-  { name: "CSS", description: "Style sheets", icon: "" },
+  {
+    name: "Javascript",
+    description: "Programming Language",
+    icon: new URL(`@/assets/skill-icon/JavaScript.svg`, import.meta.url).href,
+  },
+  {
+    name: "Node.Js",
+    description: "Javascript runtime",
+    icon: new URL(`@/assets/skill-icon/Node.js.svg`, import.meta.url).href,
+  },
+  {
+    name: "Golang",
+    description: "Programming language",
+    icon: new URL(`@/assets/skill-icon/Go.svg`, import.meta.url).href,
+  },
+  {
+    name: "PHP",
+    description: "Programming language",
+    icon: new URL(`@/assets/skill-icon/PHP.svg`, import.meta.url).href,
+  },
+  {
+    name: "Python",
+    description: "Programming language",
+    icon: new URL(`@/assets/skill-icon/Python.svg`, import.meta.url).href,
+  },
+  {
+    name: "Express.JS",
+    description: "Javascript Framework",
+    icon: new URL(`@/assets/skill-icon/Express.svg`, import.meta.url).href,
+  },
+  {
+    name: "Fiber",
+    description: "Golang Framework",
+    icon: new URL(`@/assets/skill-icon/fiber.png`, import.meta.url).href,
+  },
+  {
+    name: "Laravel",
+    description: "PHP Framework",
+    icon: new URL(`@/assets/skill-icon/Laravel.svg`, import.meta.url).href,
+  },
+  {
+    name: "Flask",
+    description: "Python Framework",
+    icon: new URL(`@/assets/skill-icon/Flask.svg`, import.meta.url).href,
+  },
+  {
+    name: "Vue.JS",
+    description: "Javascript Framework",
+    icon: new URL(`@/assets/skill-icon/Vue.js.svg`, import.meta.url).href,
+  },
+  {
+    name: "HTML",
+    description: "Markup Framework",
+    icon: new URL(`@/assets/skill-icon/HTML5.svg`, import.meta.url).href,
+  },
+  {
+    name: "CSS",
+    description: "Style sheets",
+    icon: new URL(`@/assets/skill-icon/CSS3.svg`, import.meta.url).href,
+  },
   {
     name: "Tailwind CSS",
     description: "Utility framework",
-    icon: "",
+    icon: new URL(`@/assets/skill-icon/Tailwind-CSS.svg`, import.meta.url).href,
   },
-  { name: "REST API", description: "Web service", icon: "" },
+  {
+    name: "REST API",
+    description: "Web service",
+    icon: new URL(`@/assets/skill-icon/api.png`, import.meta.url).href,
+  },
   {
     name: "Google Cloud Platform",
     description: "Cloud services",
-    icon: "googlecoud",
+    icon: new URL(`@/assets/skill-icon/Google-Cloud.svg`, import.meta.url).href,
   },
-  { name: "Amazon Web Service", description: "Cloud services", icon: "" },
-  { name: "Docker", description: "Container platform", icon: "" },
-  { name: "Git", description: "Version control", icon: "" },
+  {
+    name: "Amazon Web Service",
+    description: "Cloud services",
+    icon: new URL(`@/assets/skill-icon/AWS.svg`, import.meta.url).href,
+  },
+  {
+    name: "Docker",
+    description: "Container platform",
+    icon: new URL(`@/assets/skill-icon/Docker.svg`, import.meta.url).href,
+  },
+  {
+    name: "Git",
+    description: "Version control",
+    icon: new URL(`@/assets/skill-icon/Git.svg`, import.meta.url).href,
+  },
 ];
 </script>
 <style></style>
